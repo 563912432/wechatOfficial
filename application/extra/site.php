62dd48d51c827bba61256f4a97e57de7288bc026
陈帅 <1301491593@qq.com>
2020-01-02 17:39:06 +0800

    'add'

---
+++ application/extra/site.php
@@ -0,0 +1,38 @@
+<?php
+
+return array (
+  'name' => 'FastAdmin123',
+  'beian' => '123123123',
+  'cdnurl' => '',
+  'version' => '1.0.1',
+  'timezone' => 'Asia/Shanghai1',
+  'forbiddenip' => '',
+  'languages' =>
+  array (
+    'backend' => 'zh-cn',
+    'frontend' => 'zh-cn',
+  ),
+  'fixedpage' => 'dashboard',
+  'categorytype' =>
+  array (
+    'default' => 'Default',
+    'page' => 'Page',
+    'article' => 'Article',
+    'test' => 'Test',
+  ),
+  'configgroup' =>
+  array (
+    'basic' => 'Basic',
+    'email' => 'Email',
+    'dictionary' => 'Dictionary',
+    'user' => 'User',
+    'example' => 'Example',
+  ),
+  'mail_type' => '1',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '10000',
+  'mail_smtp_pass' => 'password',
+  'mail_verify_type' => '2',
+  'mail_from' => '10000@qq.com',
+);
